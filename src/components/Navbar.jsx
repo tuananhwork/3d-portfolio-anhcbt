24b3867d137e39c1674685650a1bb85b7acfe119
anhcbt <chubatuananh.work@gmail.com>
2023-12-25 15:56:09 +0700

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-20 h-8 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">AnhCBT</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({ isActive }) => (isActive ? 'text-blue-500' : 'text-black')}>
+          About
+        </NavLink>
+      </nav>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/projects" className={({ isActive }) => (isActive ? 'text-blue-500' : 'text-black')}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
